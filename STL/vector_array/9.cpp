70e7c84be312b6e8acd1e7b12644118408bd4189
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-07-22 11:14:35 +0900

    Update and rename 9?.cpp to 9.cpp

--- STL/vector_array/9.cpp
+++ STL/vector_array/9.cpp
@@ -9,8 +9,15 @@
 #include <vector>
 #include <algorithm>
 int main(int argc, const char * argv[]) {
-    std::vector<int> nums{0,1,2,3,4,5,6};
+    std::vector<int> nums{0,1,2,3,4,5,6}; //size 7
     nums.erase(std::remove_if(nums.begin(),nums.end(),[](int n){
+    //std::remove if란
+    //begin부터 end까지 모든 짝수 엘레멘트 삭제 그리고 마지막 엘레멘트 리턴
+    //여전히 사이즈는 7
+    
+    //erase란    
+    //리턴받은 마지막 엘레멘트부터 end까지 사이즈 삭제
+    //사이즈3
             if(n%2==0)//짝수
             {
                 return true; //짝수인것들 골라서 지우는거
