26b744a5074729998e079618e0f48993fdc3c274
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-07-22 10:36:43 +0900

    Update 1.cpp

--- STL/vector_array/1.cpp
+++ STL/vector_array/1.cpp
@@ -24,11 +24,11 @@ int main(int argc, const char * argv[]) {
     
     //벡터기본(벡터는 힙공간 배열을 가리키는 포인터와, 사이즈, 캐패시티 정보로 구성된다)
     std::vector<int> numss{0,1,2,3,4};
-    std::cout<<nums.size()<<std::endl; //5
+    std::cout<<numss.size()<<std::endl; //5
     numss.emplace_back(5);
-    std::cout<<nums.size()<<std::endl; //6
+    std::cout<<numss.size()<<std::endl; //6
     numss.pop_back();
-    std::cout<<nums.size()<<std::endl; //5
+    std::cout<<numss.size()<<std::endl; //5
     
     //벡터값 추출하는 세가지 방법
     //1.index based ((3이 제일 편하고 빠르지만 벡터 사이즈가 변화할 시 무조건 1번 써야한다))
@@ -37,12 +37,12 @@ int main(int argc, const char * argv[]) {
     }std::cout<<std::endl;
     
     //2.iterator based
-    for(auto itr=nums.begin(); itr!=nums.end(); itr++){
+    for(auto itr=numss.begin(); itr!=numss.end(); itr++){
         std::cout<<(*itr)<<" ";
     }std::cout<<std::endl;
     
     //3.range based loop (제일빠름)
-    for(const int& num : nums){
+    for(const int& num : numss){
         std::cout<<num<<" ";
     }std::cout<<std::endl;
 }
