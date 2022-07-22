fc9973b052259f7c193630235a699319a0d6ff0c
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-07-22 21:29:52 +0900

    Update and rename 1_reference.cpp to reference.cpp

--- basic/reference.cpp
+++ basic/reference.cpp
@@ -24,6 +24,7 @@ int main(int argc, const char * argv[]) {
     const Cat kitty{"kitty"};
     
     const std::string& name = kitty.name();
+    //원랜 반환되며 임시객채 거치는데 레퍼런스 리턴은 임시객체를 거치지 않는다는 걸로 받아들여도 되는건가??
     
     //std::string name = kitty.name();
     return 0;
