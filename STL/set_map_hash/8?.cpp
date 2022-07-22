0a0a036b14a02df1a85577e5b9d984c1b6fe5319
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-07-22 13:24:55 +0900

    Update 8?.cpp

--- STL/set_map_hash/8?.cpp
+++ STL/set_map_hash/8?.cpp
@@ -21,19 +21,19 @@ int main(int argc, const char * argv[]) {
     uordSet.emplace("ghi");
     uordSet.emplace("jkl");
     
-    std::cout<<"abc: "<<std::hash<std::string>{}("abc")<<std::endl; //해시값 출력
-    std::cout<<"def: "<<std::hash<std::string>{}("def")<<std::endl;
-    std::cout<<"ghi: "<<std::hash<std::string>{}("ghi")<<std::endl;
-    std::cout<<"jkl: "<<std::hash<std::string>{}("jkl")<<std::endl;
-    //해쉬값이 모두 다른데, 각 문자열이 해쉬평션을 통하면 저 해쉬값들이 나옴
+    std::cout<<"abc: "<<std::hash<std::string>{}("abc")<<std::endl; //해시값 출력 ex)473284729348
+    std::cout<<"def: "<<std::hash<std::string>{}("def")<<std::endl; //해시값 출력 ex)4732847293434
+    std::cout<<"ghi: "<<std::hash<std::string>{}("ghi")<<std::endl; //해시값 출력 ex)47328472923
+    std::cout<<"jkl: "<<std::hash<std::string>{}("jkl")<<std::endl; //해시값 출력 ex)473284729356
+    //해쉬값이 모두 다르다. 각 문자열이 해쉬평션을 통하면 저 해쉬값들이 나옴
     
-    //근데 해쉬값이 너무 큼 -> 버킷 카운트에 해쉬값 담아놈
+    //근데 해쉬값이 너무 큼 -> 버킷 카운트에 해쉬값 담아놈 (0,1,2,3 등등 과 같이 간소화되는 해쉬값)
     std::cout<<uordSet.bucket_count()<<std::endl; //13?
     //해쉬값들이 5개의 버킷중에 몇번째 버킷에 들어갔는지도 알 수 있음
     std::cout<<"abc: "<<uordSet.bucket("abc")<<std::endl; //4번째 bucket
-    std::cout<<"def: "<<uordSet.bucket("abc")<<std::endl; //7
-    std::cout<<"ghi: "<<uordSet.bucket("abc")<<std::endl; //4
-    std::cout<<"jkl: "<<uordSet.bucket("abc")<<std::endl; //8
+    std::cout<<"def: "<<uordSet.bucket("def")<<std::endl; //7
+    std::cout<<"ghi: "<<uordSet.bucket("ghi")<<std::endl; //4
+    std::cout<<"jkl: "<<uordSet.bucket("jkl")<<std::endl; //8
     
     //키값으로 해쉬값 찾는시간, 해쉬값으로 버킷카운트넘버 구하는 시간, 그 버킷카운트에서 링크드리스트 타면서 추가/삭제 하는 시간은 0(1)
     
