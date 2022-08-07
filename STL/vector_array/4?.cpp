af797fc8880ae843e80aad0928e1b64ba33116e1
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-08 00:13:08 +0900

    Update 4?.cpp

--- STL/vector_array/4?.cpp
+++ STL/vector_array/4?.cpp
@@ -20,7 +20,7 @@ int main(int argc, const char * argv[]) {
     cats.emplace_back(Cat{"kitty"}); //스택위에 Cat객체의 임시객체가 만들어지고 이 값들이 백터의 마지막 공간으로 move됨!
     cats.emplace_back(Cat{"nabi"}); //move process가 있다
     
-    //move process없게
+    //move process없게 바로 백터공간에 값이 들어간다.
     Cat& cat=cats.emplace_back("changmin"); //g++ 4.cpp -std=c++17
     
     Cat nabi{"nabi"};// Lvalue = copy
