0baf1f76c189933cd160ee0b1d8ad2561474c32f
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-14 00:39:45 +0900

    Update 2_defineLamda.cpp

--- lamda/2_defineLamda.cpp
+++ lamda/2_defineLamda.cpp
@@ -18,32 +18,35 @@ private:
 
 int main(int argc, const char * argv[]) {
     //lambdaPlus3 정의1
+    //[] : 캡쳐모드
     auto lambdaPlus3 = [localVar = 3] (int x){ //[]는 {}안에 들어갈 지역변수느낌, ()는 매개변수
+  //auto lambdaPlus3 = [localVar = 3] (int x,int y){ //매개변수 2개
         return localVar + x;
     };
     
-    //lambdaPlus3 정의2
+    //lambdaPlus3 capture by value
     int three=3;
     auto lambdaPlus3 = [three] (int x){
         return three + x;
     };
     
-    //lambdaPlus3 정의3
+    //lambdaPlus3 capture by reference
+    //매우 큰 오브젝트가 힙에 있고 pointing하는 그림
     veryLargeObj object; //in heap
     auto lambdaPlus3 = [&object] (int x){
         return object + x;
     };
     
-    //lambdaPlus3 정의4
+    //lambdaPlus3 auto capture by value
     auto lambdaPlus3 = [=] (int x){
-        //스스로으로 value를 캡쳐함
+        //스스로으로 local value를 캡쳐함(만약 three야? 그럼 지역변수 three 캡처 하는거)
         return localVar + x;
     };
     
-    //lambdaPlus3 정의5
+    //lambdaPlus3 auto capture by reference
     auto lambdaPlus3 = [&] (int x){
         //스스로으로 reference를 캡쳐함
-        return localVar + x;
+        return reference + x;
     };
     
     Plus plus3{3};
