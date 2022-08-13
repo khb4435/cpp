2b769e118afde93901e38efc4dfa1c0e0414f81d
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-14 02:41:05 +0900

    Update 1.cpp

--- modernC++/LRvalue/1.cpp
+++ modernC++/LRvalue/1.cpp
@@ -23,3 +23,27 @@ int main(){
     storeByRRef("abc");
     storeByRRef(std::move(a));
 }
+
+-------------------std::move 오너십
+//1
+void storeByLRef(std::string &s){
+    std::string b = std::move(s);
+};
+
+int main(){
+    std::string a = "abc";
+    storeByLRef(a);
+    std::cout<<a<<std::endl; //값이 없음 오너십 b에뺏김
+}
+
+//2
+void storeByLRef(const std::string &s){
+    std::string b = std::move(s);
+};
+
+int main(){
+    std::string a = "abc";
+    storeByLRef(a);
+    std::cout<<a<<std::endl; //"abc" const , std::move조합은
+    //copy일어난다.
+}
