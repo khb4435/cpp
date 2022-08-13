721424f602bb4343e3181164b6ba18842825ac74
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-14 02:54:08 +0900

    Create 2.cpp

---
+++ modernC++/LRvalue/2.cpp
@@ -0,0 +1,27 @@
+#include <string>
+//value,lvalue rvalue 셋 다 받으면서 
+//lvalue넘어갈땐 1copy
+//rvalue넘어갈땐 0copy
+//가 목표
+
+//결론 값으로 받고 std::move
+
+//일단 함수의 인자는 lref,rref를 해버리면 둘 중 하나만 
+//받을 수 있어서 한정됨
+
+class Cat{
+    public:
+        void setName(std::string name){
+            mName = std::move(name);
+        }
+    private:
+        std::string mName;
+}
+
+int main(){
+    Cat kitty;
+    std::string s = "kitty";
+
+    kitty.setName(s);//1copy
+    kitty.setName("kitty"); //0copy
+    //복사생략을 통해 name이 "kitty"가리킨다.
