e0901e3b9c7e5039e49072c6ff7d16961d54fe96
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-15 22:25:05 +0900

    Update 9_variant(safe union).cpp

--- STL/types/9_variant(safe union).cpp
+++ STL/types/9_variant(safe union).cpp
@@ -1,10 +1,5 @@
-//
-//  main.cpp
-//  variant
-//
-//  Created by 김현배 on 2022/03/01.
-//
-
+//c++17
+//안전한 union임.
 #include <iostream>
 #include <variant>
 struct S{
@@ -19,7 +14,8 @@ union U{
 
 int main(int argc, const char * argv[]) {
     std::variant<int,double> v;
-    std::cout<<sizeof(v)<<std::endl; //Union같은 메모리구조 하지만 무슨 메모미인지 추적하는 8바이트 추가
+    std::cout<<sizeof(v)<<std::endl; //Union같은 메모리구조 
+    //하지만 현재 어떤 메모리인지 추적하는 8바이트 추가
     std::cout<<sizeof(S)<<std::endl; //16
     std::cout<<sizeof(U)<<std::endl; //8
     return 0;
