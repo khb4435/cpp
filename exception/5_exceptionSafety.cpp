d9164276fd92a18d9386c9f1a34948d5213318b2
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-15 23:54:48 +0900

    Update and rename 5_safetyGaruntee.cpp to 5_exceptionSafety.cpp

--- exception/5_exceptionSafety.cpp
+++ exception/5_exceptionSafety.cpp
@@ -7,7 +7,8 @@
 
 //exception safety : destrucotr, swap, move,defualt constrotor등은 절대 예외처리 throw 하면안됨
 //:no except붙여주기
-//3.no throw garuntee
+//3.no throw garuntee : exception이 던져지지 않은 함수
+
 #include <iostream>
 #include <exception>
 int g=0;
@@ -30,7 +31,7 @@ void f(){
     try{
         int a = divide(10,0);
         g=a;
-    }//main까지 exception이 propagated되지 않음
+    }//main까지 exception이 propagated되지 않음!!!!
     catch(const std::exception(&e))
     {
     }
