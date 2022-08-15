fe8255710b606a1433ab6a2941677690b9c9087c
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-15 22:29:45 +0900

    Update 8_union.cpp

--- STL/types/8_union.cpp
+++ STL/types/8_union.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
  
-//메모리를 소멸자 써주면서 관리하기 불편.
+//메모리를 소멸자/생성자 써주면서 관리하기 불편.
 //type tracking
 //union-like class, tagged union 검색
 //std::variant가 union보다 낫다.
@@ -27,3 +27,4 @@ int main()
 }
 
 //동적할당되는 메모리의 경우 메모리 덮어쓰기전에 소멸자 호출!
+//새로 만들때 생성자 호출!
