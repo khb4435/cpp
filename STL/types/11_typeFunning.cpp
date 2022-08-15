28173b357006ba3ce84a1450bc38392c2626bb2e
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-15 22:47:21 +0900

    Update and rename main.cpp to 11_typeFunning.cpp

--- STL/types/11_typeFunning.cpp
+++ STL/types/11_typeFunning.cpp
@@ -10,12 +10,16 @@
 #include <cstring>
 #include <bit>
 #include <cstdint>
+
 //float으로 쓰고 unsigned int로 읽는 예제코드
 bool isNeg(float x){
     //return x < 0.0f; -> 느림
     
-    //이런건 되는거 있고 안되는 컴파일러 있고 그럼
-    //unsigned char*, char*, std::bite* 만 모든 컴파일러에서 적용됨
+    //이 방식은 되는거 있고 안되는 컴파일러 있고 그럼
+    //unsigned char*, char* , std::bite* 만 모든 컴파일러에서 적용됨
+    
+    //float로 비교하는게 느리니까 unsigned int로 받아서 비교
+    //float의 첫비트 부호비트 비교
     //unsigned int* ui = (unsigned int*)&x;
     //return *ui & 0x80000000;
     
