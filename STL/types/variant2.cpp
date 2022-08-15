f0da0cebc12801283781a9556c9680605940aa43
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-15 22:27:38 +0900

    Update variant2.cpp

--- STL/types/variant2.cpp
+++ STL/types/variant2.cpp
@@ -17,16 +17,15 @@ int main(int argc, const char * argv[]) {
     std::variant<int,double,float> v;
     v=10; //int타입
     
+    //union문제1
     //타입에 맞지 않으면 접근하지 않는 varinat !!
+    //expection던져지면서 프로그램멈춤
+    //excetion싫으면
     if(auto pVal = std::get_if<double>(&v)){
         std::cout<<*pVal<<std::endl;
     }
     else{
         std::cout<<"v is not double type"<<std::endl; //출력됨
     }
-    
-    U u;
-    u.i=10;
-    std::cout<<u.d<<std::endl; //그냥 유니언을 쓰면 이상한 값 출력됨.
     return 0;
 }
