0ede9abed0262fc7ffede9ea4c057dab5107dcd8
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-15 22:15:10 +0900

    Update and rename union.cpp to 7_union.cpp

--- STL/types/7_union.cpp
+++ STL/types/7_union.cpp
@@ -15,8 +15,8 @@ struct S{ //4 + (4) + 8 = 16
 };
 
 union U{ //제일 큰 메모리 공간을 공유 8바이트
-    int i4;
-    double d8;
+    int i4; //4
+    double d8; //8
 };
 
 int main(int argc, const char * argv[]) {
@@ -25,12 +25,14 @@ int main(int argc, const char * argv[]) {
     
     U u;
     u.i4 = 10;
-    std::cout<<u.i4<<std::endl;
+    std::cout<<u.i4<<std::endl; //10
     
-    u.d8=0.3;
-    std::cout<<u.d8<<std::endl;
+    u.d8=0.3; //0.3으로 덮혀써짐
+    std::cout<<u.d8<<std::endl; //0,3
     
-    std::cout<<u.i4<<std::endl; //전혀 상관없는 숫자 출력됨
+    std::cout<<u.i4<<std::endl; //접근은 가능한데,
+    //전혀 상관없는 숫자 출력됨
+    //즉 0.3이 8바이트내 이진수로 쓰여지는데 여기서 4바이트만 가져오는 꼴 
     //type punning을 하지 않는 이상 union 사용 절대 금지
     return 0;
 }
