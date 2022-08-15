0ba1b9762717f5e214a4860016a5493017e3063f
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-15 23:17:05 +0900

    Update and rename 1.cpp to 1_예외처리란.cpp

--- exception/1_예외처리란.cpp
+++ exception/1_예외처리란.cpp
@@ -6,7 +6,7 @@
 //
 
 
-//C에는 예외처리가 없고
+//C에는 예외처리가 없어서 모든 에러를 리턴으로 해결
 //C++에는 optional,pair,tuple등 error return하는 방법외에도
 //exception(try,catch,throw)가 있음
 
@@ -20,10 +20,13 @@ public:
 
 int divide(int a, int b){
     if(b==0){
-        //throw::std::exception(); //std::exception이라는 객체를 만들어서 던짐
+        //catch1
+        throw::std::exception(); //std::exception이라는 객체를 만들어서 던짐
+        //std::exception의 자식클래스
+        throw::std::runtime_error("divide by 0");
         
-        //throw::std::runtime_error("divide by 0");
-        
+        //catch2
+        //excetion등이 객체라고 했는데, 객체 던지기 가능
         throw Cat();
     }
     return a/b;
@@ -32,20 +35,18 @@ int main(int argc, const char * argv[]) {
     try{
         std::cout<<divide(10,0)<<std::endl;
     }
-    /*
-    catch(const std::exception(&e)){
-        //std::cout<<"std::exception is catched"<<std::endl;
-        
+    //catch 1
+    catch(const std::exception &e){
         std::cout<<e.what()<<std::endl;
         std::cout<<"std::exception is catched"<<std::endl;
     }
+    //catch 2
     catch(const Cat& c){
         std::cout<<"cat is catched"<<std::endl;
     }
-     */
+    
+    //어떤 throw된 객체 다 받는다.
     catch(...){
-        std::cout<<"std::exception is catched"<<std::endl;
-        //std::cout<<e.what()<<std::endl;
         std::cout<<"std::exception is catched"<<std::endl;
         std::cout<<"cat is catched"<<std::endl;
     }
