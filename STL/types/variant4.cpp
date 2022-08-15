6c6012e280aebec307f1c1ebf30f01710f39f52c
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-15 22:32:22 +0900

    Update variant4.cpp

--- STL/types/variant4.cpp
+++ STL/types/variant4.cpp
@@ -9,13 +9,14 @@
 
 //optional, pair, tuple
 
-//int와 Enum class가 메모리를 공유함에도 불구하고 pair, tuple과 아무 차이없이 이용가능하다
-//std::variant<int,Error_code> double(int a,int b){
-//    if(b==0){
-//        return Error_code::divide0
-//    }
-//    return a/b;
-//};
+//반환값에 int와 Enum class가 메모리를 공유함에도 불구하고 pair, tuple과 아무 차이없이 이용가능하다
+
+std::variant<int,Error_code> double(int a,int b){
+    if(b==0){
+        return Error_code::divide0
+    }
+    return a/b;
+};
 
 #include <iostream>
 #include <variant>
