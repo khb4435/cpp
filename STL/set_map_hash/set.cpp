5c9994fa8c34c239fbca6c3a083dc8d552eedacb
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-15 20:29:25 +0900

    Update set.cpp

--- STL/set_map_hash/set.cpp
+++ STL/set_map_hash/set.cpp
@@ -6,10 +6,13 @@
 //
 
 
-//set/map -> 내부적 tree구조, 0(logN), operator< , sorting
-//unordered set/map -> hash구조, 0(1), operator=, no sorting
+//set -> 내부적 tree구조(BSTorRed black tree), find,삽입,삭제 모두 0(logN)
+//map(set+(key,value)) -> 내부적 tree구조(BSTorRed black tree), find,삽입,삭제 모두 0(logN)
+
+//unordered set/map -> hash구조, find,삽입,삭제 모두 0(1), operator=, no sorting
 //set/unordered set -> key
 //map/unordered map -> key and value
+
 //중복이고 싶으면 multi 붙여주면됌
 #include <iostream>
 #include <set>
