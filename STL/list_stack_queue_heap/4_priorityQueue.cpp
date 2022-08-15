676da8d6ee7612b1ec39847746de4b1bb5c837fa
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-15 19:33:28 +0900

    Update and rename priority_queue.cpp to 4_priorityQueue.cpp

--- STL/list_stack_queue_heap/4_priorityQueue.cpp
+++ STL/list_stack_queue_heap/4_priorityQueue.cpp
@@ -9,9 +9,15 @@
 
 //queue : first in first out
 //priority queue : pop이 되는 엘레멘트에 우선순위가 있음
-//MAX값이 무조건 top으로 가는 트리구조!!!
-//삽입/삭제 : 0(logN) -> tree구조를 갖는다.
+//MAX값이 무조건 top으로 가는 트리구조
+//삽입되면 가장 오른쪽 끝에 삽입되고 트리구조의 룰을 지키기위해
+//부모자식간 스왑과정을 거치게된다.
+//insertion/pop : 0(logN) -> tree구조를 갖는다.
 // MIN/MAX : 0(1)
+//top을 찍기만 하면 그게 max
+//tree구조로 부모가 항상 자식보다 크고, 
+//왼쪽부터 차 나간다.
+
 #include <iostream>
 #include <queue>
 int main(int argc, const char * argv[]) {
@@ -26,8 +32,9 @@ int main(int argc, const char * argv[]) {
     
     std::cout<<nums.top()<<std::endl; //9
     
-    //0(logN) 새로운 element가 top까지 도달해야 하기 때문
+    //0(logN) 새로운 element(10)가 top까지 도달해야 하기 때문
     nums.emplace(10);
+    
     //0(1) : top만 찍으면됌
     std::cout<<nums.top()<<std::endl; //10
     
