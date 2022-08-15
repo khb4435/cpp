2232e0321ec5e034a588e08ecc36aec12cb03f54
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-15 22:07:31 +0900

    Update 5_optional.cpp

--- STL/types/5_optional.cpp
+++ STL/types/5_optional.cpp
@@ -10,6 +10,6 @@
 
 int main(int argc, const char * argv[]) {
     int num=10;
-    std::optional<int> num2{20};//4바이트보다 큼 메모리의 존재 유무 정보도 포함한다.
+    std::optional<int> num2{20};//int형 메모리+valid / or not valid 정보
     return 0;
 }
