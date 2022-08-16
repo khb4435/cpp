dfa5947f92f7dc34d5d92011d3845415f29dc85b
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-08-16 22:29:06 +0900

    Update and rename various_templete.cpp to 4_variousTemplete.cpp

--- templete/4_variousTemplete.cpp
+++ templete/4_variousTemplete.cpp
@@ -10,12 +10,14 @@
 
 #include <iostream>
 #include <vector>
+
 //class template
 template <typename T>
 class Stack{
 public:
     void push(T elem){
-        //push특성상 lvalue일때는 카피, Rvalue일땐 move를 일으키기위해 이렇게 했다함
+//예제 특성상 push 로 들어오는 element는 R-value일 경우는 내부벡터로 move가 되야하고, L-value라면 복사가 되야하기 때문에 pass by value로 만들었습니다.  
+//R value로 넘어올때만 &&와 같습니다.
         mVec.emplace_back(std::move(elem));
     }
     bool pop(T& elem){
@@ -30,7 +32,7 @@ private:
     std::vector<T> mVec;
 };
 
-//aliaing template
+//aliaing(별명) template
 //c++17이전에는 typedef 이후에는 using쓰자
 template<typename T>
 using nocopeKeys = std::vector<std::array<T,64>>;
@@ -54,7 +56,7 @@ int main(int argc, const char * argv[]) {
     //aliaing template
     //c++17이전에는 typedef 이후에는 using쓰자
     nocopeKeys<float> floatKeys;
-    //std::vector<std::array<float,64>>;
+    //=std::vector<std::array<float,64>>;
     
     //variable template
     int intpi = pi<int>;
