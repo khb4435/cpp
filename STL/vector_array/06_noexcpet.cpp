77960079081490ab344532dd585272f417cea294
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-09-06 09:15:56 +0900

    Update and rename 6.cpp to 06_noexcpet.cpp

--- STL/vector_array/06_noexcpet.cpp
+++ STL/vector_array/06_noexcpet.cpp
@@ -1,13 +1,6 @@
-//
-//  main.cpp
-//  6
-//
-//  Created by 김현배 on 2022/02/20.
-//
-
-//move cons/assignment 나 destructor처럼 절대 exception이 나지 않는 곳에 수행되야하는 놈들은 noexcept를 붙여줘야 optimization이 됩니다.
+//move cons/assignment 나 destructor처럼 절대 exception이 일어나면 안되는 곳에 수행되야하는 놈들은 noexcept를 붙여줘야 optimization이 됩니다.
 //emplace_back시 다른공간있으면 다른공간에 할당되는데 copy보다 move로 해결하기 위해
-//move생성자정의
+//move생성자 정의
 //다시한번 애초에 이런일 안 일어나도록 큰 공간 reserve해주자
 
 #include <iostream>
