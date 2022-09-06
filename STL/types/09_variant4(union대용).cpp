2df35064a3598349ccbe2f1a4449fbb812818076
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-09-06 11:10:13 +0900

    Update and rename 9_variant4.cpp to 09_variant4(union대용).cpp

--- STL/types/09_variant4(union대용).cpp
+++ STL/types/09_variant4(union대용).cpp
@@ -1,14 +1,3 @@
-//
-//  main.cpp
-//  variant4
-//
-//  Created by 김현배 on 2022/03/01.
-//
-
-
-
-
-
 //반환값에 int와 Enum class가 메모리를 공유함에도 불구하고 pair, tuple과 아무 차이없이 이용가능하다
 
 std::variant<int,Error_code> double(int a,int b){
