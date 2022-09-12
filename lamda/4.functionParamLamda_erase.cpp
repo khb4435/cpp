158d556534ed822fd3c973a787b40f2e5ac1ef5b
khb4435 <79688282+khb4435@users.noreply.github.com>
2022-09-12 21:44:26 +0900

    Update 4.functionParamLamda_erase.cpp

--- lamda/4.functionParamLamda_erase.cpp
+++ lamda/4.functionParamLamda_erase.cpp
@@ -1,17 +1,10 @@
-//
-//  main.cpp
-//  erase
-//
-//  Created by 김현배 on 2022/02/20.
-//
-
-
 //higher order function
 //하나 혹은 두개의 함수를 파라미터를 받는다.
 //함수를 결과로 리턴한다.
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 int main(int argc, const char * argv[]) {
     std::vector<int> nums {1,2,3,4,5,6,7,8,9,10};
     
@@ -19,7 +12,7 @@ int main(int argc, const char * argv[]) {
         n+=10;
     };
     
-    //std::for_each는 처음부터 끝까지 모든 원소가 함수를 받게함.
+    //std::for_each는 처음부터 끝까지 모든 원소가 "함수"를 받게함.
     //1
     std::for_each(nums.begin(),nums.end(),lamdaAdd10);
     
